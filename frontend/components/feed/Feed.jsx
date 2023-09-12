1373876d53f1d71c7283ee2cf3fd931b3e780342
Julian Festing <julfesting@gmail.com>
2023-09-12 13:20:41 +0100

    finished input for adding meal deal and increasing total

--- frontend/components/feed/Feed.jsx
+++ frontend/components/feed/Feed.jsx
@@ -4,22 +4,22 @@ import Input from '../input/Input';
 import StreakTracker from "../../classes/StreakTracker";
 import Total from "../../classes/Total";
 
+const totalCounter = new Total();
+
 function Feed() {
 	const [streakTracker] = useState(new StreakTracker());
-	const [total, setTotal] = useState(new Total());
+
+  const [total, setTotal] = useState(totalCounter.getTotal());
 
   const handleMealDealConsumed = () => {
-    setTotal(prevTotal => {
-      prevTotal.incrementTotal(); // Use the function to update the state
-      return { ...prevTotal }; // Return the updated state
-    });
-    streakTracker.incrementStreak();
+    totalCounter.incrementTotal();
+    setTotal(instantiatedClass.getTotal())
   };
 
   return (
     <>
       <StreakCounter
-        total={total.getTotal()}
+        total={total}
         currentStreak={streakTracker.getCurrentStreak()}
         longestStreak={streakTracker.getLongestStreak()}
       />
