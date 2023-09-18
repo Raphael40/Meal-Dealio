9f5fa7e7eaf4231628a41e669be55611fc93ea96
Julian Festing <julfesting@gmail.com>
2023-09-18 14:37:40 +0100

    configured the firbase connection, wrote useEffect to pull ddata from collection

--- frontend/components/feed/Feed.jsx
+++ frontend/components/feed/Feed.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
 import StreakDisplay from '../streak-display/StreakDisplay';
 import Input from '../input/Input';
 import StreakTracker from "../../classes/StreakTracker";
 import Total from "../../classes/Total";
+
+import { db } from '../../firebase/config'
+import { collection, getDocs } from 'firebase/firestore'
 
 const totalCounter = new Total();
 const streakTracker = new StreakTracker()
@@ -11,6 +15,25 @@ function Feed() {
   const [total, setTotal] = useState(totalCounter.getTotal());
 	const [currentStreak, setCurrentStreak] = useState(streakTracker.getCurrentStreak());
   const [longestStreak, setLongestStreak] = useState(streakTracker.getLongestStreak());
+
+  useEffect(() => {
+    async function fetchData() {
+      const ref = collection(db, 'Meals');
+  
+      try {
+        const snapshot = await getDocs(ref);
+        let results = [];
+        snapshot.docs.forEach(doc => {
+          results.push({id: doc.id, ...doc.data()});
+        });
+        console.log(results);
+      } catch (error) {
+        console.error('Error fetching documents: ', error);
+      }
+    }
+  
+    fetchData();
+  }, []);
 
   const handleMealDealConsumed = () => {
     totalCounter.incrementTotal();
