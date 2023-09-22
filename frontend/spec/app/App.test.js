ae60d2f7a6943bc27f392273b3094ad3a497d348
Julian Festing <julfesting@gmail.com>
2023-09-22 17:32:29 +0100

    passed Input component test

--- frontend/spec/app/App.test.js
+++ frontend/spec/app/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {jest} from '@jest/globals';
 import renderer from 'react-test-renderer';
 
 import App from '../../App';
---
+++ frontend/spec/components/Input.test.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+import { render, fireEvent, } from '@testing-library/react-native';
+import {jest} from '@jest/globals';
+import Input from '../../components/input/Input';
+
+describe('Input component', () => {
+  it('should render the componenet and call handleMealDealConsumed when the button is pressed', async () => {
+    const mockHandleMealDealConsumed = jest.fn();
+
+    const { getByText } = render(
+      <Input handleMealDealConsumed={mockHandleMealDealConsumed} />
+    );
+
+    const button = getByText('Add Meal Dealio');
+    fireEvent.press(button);
+
+    await Promise.resolve();
+
+    expect(mockHandleMealDealConsumed).toHaveBeenCalled();
+  })
+})
