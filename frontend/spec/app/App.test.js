853f91fed2c8c594cc0a41afc4dba71216ecf481
JRSkates <skatesproduction@gmail.com>
2023-09-25 16:15:22 +0100

    fixed app.js test

--- frontend/spec/app/App.test.js
+++ frontend/spec/app/App.test.js
@@ -9,6 +9,6 @@ describe('<App />', () => {
 
   it('has 1 child', () => {
     const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(2);
+    expect(tree.children.length).toBe(1);
   });
 });
