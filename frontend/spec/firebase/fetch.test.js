70f74313158c8d7ed67f79bf25dc3d04953d2c48
Julian Festing <julfesting@gmail.com>
2023-09-25 21:49:32 +0100

    Added comments to fetch.test.js

--- frontend/spec/firebase/fetch.test.js
+++ frontend/spec/firebase/fetch.test.js
@@ -2,6 +2,7 @@ import putRequest from "../../firebase/fetch";
 import { getFirestore, doc, updateDoc } from 'firebase/firestore'; // Import Firestore functions
 import { db } from "../../firebase/config";
 
+// Mock the Firebase functions
 jest.mock('firebase/firestore', () => ({
   getFirestore: jest.fn(),
   doc: jest.fn(),
@@ -9,6 +10,7 @@ jest.mock('firebase/firestore', () => ({
 }));
 
 describe('putRequest', () => {
+  // Mock the console.error function
   const originalConsoleError = console.error;
   beforeAll(() => {
     console.error = jest.fn();
@@ -18,6 +20,7 @@ describe('putRequest', () => {
   });
   
   it('should update the document in Firestore', async () => {
+    // Mock the Firestore functions
     const mockedDocRef = doc(db, 'Meals', '8rhTTawNno5YJWVbHkfI')
     doc.mockReturnValue(mockedDocRef);
 
@@ -25,8 +28,10 @@ describe('putRequest', () => {
     const current = 5;
     const longest = 7;
 
+    // Call the function
     await putRequest(total, current, longest);
 
+    // Check if the function has been called with the correct arguments
     expect(updateDoc).toHaveBeenCalledWith(mockedDocRef, {
       total: total,
       currentStreak: current,
@@ -35,24 +40,31 @@ describe('putRequest', () => {
   });
 
   it('should handle errors', async () => {
+    // Mock the Firestore functions
     const mockedDocRef = doc(db, 'Meals', '8rhTTawNno5YJWVbHkfI')
     doc.mockReturnValue(mockedDocRef);
 
+    // Mock the updateDoc function to throw an error
     const error = new Error('there was an error');
     updateDoc.mockRejectedValue(error);
 
+    // Mock the console.error function
     const consoleErrorSpy = jest.spyOn(console, 'error');
 
+    // Call the function
     await putRequest(10, 5, 7);
 
+    // Check if the function has been called with the correct arguments
     expect(updateDoc).toHaveBeenCalledWith(mockedDocRef, {
       total: 10,
       currentStreak: 5,
       longestStreak: 7,
     });
 
+    // Check if the console.error function has been called with the correct arguments
     expect(consoleErrorSpy).toHaveBeenCalledWith('there was an error', error);
 
+    // Restore the console.error function
     consoleErrorSpy.mockRestore();
   })
 });
